1268237db5240913ff612a17b74dc4fac49fa19e
Sujit Kumar <sujit@zerotoone.ai>
2025-07-14 06:21:44 +0530

    deployment with celery together

--- crud.py
+++ crud.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from . import models, schemas
+import models, schemas
 
 def get_edit(db: Session, edit_id: int):
     return db.query(models.Edit).filter(models.Edit.id == edit_id).first()
--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, DateTime, text
-from .database import Base
+from database import Base
 import datetime
 import uuid
 
